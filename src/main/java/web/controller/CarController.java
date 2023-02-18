64e38b2b8132c18295a7d8f3655a5304a6d410b2
Natalya Botyasova <mrrnyash@gmail.com>
2023-02-18 18:01:43 +0800

    Fixed CarController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
 
-    CarService carService = new CarServiceImpl();
+    private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(value = "count", defaultValue = "5") Integer count) {
