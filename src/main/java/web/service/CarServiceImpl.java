e27e11f3660ab35f94e87b030a4387ac7857a85b
Natalya Botyasova <mrrnyash@gmail.com>
2023-02-19 22:21:41 +0800

    Modified Car Service Implementation

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,25 +3,24 @@ package web.service;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    private static final List<Car> cars = new ArrayList<>();
-
-    static {
-        cars.add(new Car(1L, "BMW", "black", 2.0));
-        cars.add(new Car(2L, "Toyota", "silver", 2.0));
-        cars.add(new Car(3L, "Renault", "brown", 1.8));
-        cars.add(new Car(4L, "Lada", "white", 1.2));
-        cars.add(new Car(5L, "Nissan", "blue", 3.0));
-    }
+    private static final List<Car> CARS = Stream.of(
+            new Car(1L, "BMW", "black", 2.0),
+            new Car(2L, "Toyota", "silver", 2.0),
+            new Car(3L, "Renault", "brown", 1.8),
+            new Car(4L, "Lada", "white", 1.2),
+            new Car(5L, "Nissan", "blue", 3.0)
+    ).toList();
+    
 
     @Override
     public List<Car> listCars(Integer count) {
-        return cars.stream().limit(count).toList();
+        return CARS.stream().limit(count).toList();
     }
 
 }
